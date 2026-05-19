c8ca47dc81781dfb7a13af7341c19238722ecb32
deltaheiwa <georgi.jolak@gmail.com>
2026-05-20 00:41:31 +0200

    fix: Changed colorscheme to moonfly

--- init.lua
+++ init.lua
@@ -402,10 +402,18 @@ do
     },
   }
 
+  vim.pack.add({
+    { src = gh 'bluz71/vim-moonfly-colors', name = 'moonfly' },
+  })
+
+
+  require 'moonfly'
+  vim.g.moonflyItalics = false
+
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'moonfly'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
