b56a19469f821bcb12159b1c60f1231c3a6bf63f
deltaheiwa <georgi.jolak@gmail.com>
2026-05-20 00:20:47 +0200

    fix: Rewrote how keybindings are set for flash.nvim plugin

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -1,47 +1,11 @@
-return {
-  'folke/flash.nvim',
-  event = 'VeryLazy',
-  opts = {},
-  keys = {
-    {
-      'q',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').jump()
-      end,
-      desc = 'Flash',
-    },
-    {
-      'Q',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').treesitter()
-      end,
-      desc = 'Flash Treesitter',
-    },
-    {
-      'r',
-      mode = 'o',
-      function()
-        require('flash').remote()
-      end,
-      desc = 'Remote Flash',
-    },
-    {
-      'R',
-      mode = { 'o', 'x' },
-      function()
-        require('flash').treesitter_search()
-      end,
-      desc = 'Treesitter Search',
-    },
-    {
-      '<c-q>',
-      mode = { 'c' },
-      function()
-        require('flash').toggle()
-      end,
-      desc = 'Toggle Flash Search',
-    },
-  },
-}
+local map = vim.keymap.set
+
+vim.pack.add { 'https://github.com/folke/flash.nvim' }
+
+map({ 'n', 'x', 'o' }, 'q', function() require('flash').jump() end, { desc = 'Flash' })
+map({ 'n', 'x', 'o' }, 'Q', function() require('flash').treesitter() end, { desc = 'Flash Treesitter' })
+map('o', 'r', function() require('flash').remote() end, { desc = 'Remote Flash' })
+map({ 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter Search' })
+map('c', '<c-q>', function() require('flash').toggle() end, { desc = 'Toggle Flash Search' })
+
+require('flash').setup({})
